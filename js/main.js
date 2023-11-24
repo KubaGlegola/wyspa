a8523c5046dccbab6a7cda112848d9e02396295b
KubaGlegola <kubaglegola@gmail.com>
2023-11-24 11:47:38 +0100

    finish project

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 const navHandler = document.querySelector(".nav__content");
 const burgerBtnHandler = document.querySelector(".burger-btn");
 const links = document.querySelectorAll(".nav__item");
+const navBtnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
 
 const handleNav = () => {
   navHandler.classList.toggle("nav__content--active");
 
   links.forEach((link) =>
     link.addEventListener("click", () => {
-      navHandler.classList.remove("nav--active");
+      navHandler.classList.remove("nav__content--active");
     })
   );
 
@@ -21,4 +24,29 @@ const handleNavItemsAnimation = () => {
   });
 };
 
+const handleCurrentYear = () => {
+  const year = new Date().getFullYear();
+  footerYear.innerText = year;
+};
+
+const handleObserver = () => {
+  const currentSection = window.scrollY;
+
+  allSections.forEach((section) => {
+    if (
+      section.classList.contains("white-section") &&
+      section.offsetTop <= currentSection + 60
+    ) {
+      navBtnBars.classList.add("black-bars-color");
+    } else if (
+      !section.classList.contains("white-section") &&
+      section.offsetTop <= currentSection + 60
+    ) {
+      navBtnBars.classList.remove("black-bars-color");
+    }
+  });
+};
+
+handleCurrentYear();
 burgerBtnHandler.addEventListener("click", handleNav);
+window.addEventListener("scroll", handleObserver);
